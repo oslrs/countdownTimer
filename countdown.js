2a2203f92ef9198782f5c17f8658e863ca2ad248
Dan Fitz <dan@danfitz.com>
2019-11-28 18:09:30 -0500

    Added params customization for timer

--- countdown.js
+++ countdown.js
@@ -1,9 +1,21 @@
-const then = moment(new Date("2019/11/29"));
-const now = moment.now();
-const diff = then.diff(now, "seconds");;
+// 1. Create URL object from window.location
+const url = new URL(window.location)
+// 2. Create URLSearchParams object from URL
+const params = new URLSearchParams(url.search)
+// 3. Get event and date from params
+const title = params.get('title')
+const date = params.get('date')
 
-const clock = $(".timer").FlipClock(diff, {
+// 4. Set 
+const titleSpan = document.querySelector('.title')
+titleSpan.textContent = title
+
+const then = moment(new Date(date))
+const now = moment.now()
+const diff = then.diff(now, 'seconds')
+
+const clock = $('.timer').FlipClock(diff, {
   countdown: true,
-  clockFace: "DailyCounter",
-  language: $(".frTag").length ? "fr-ca" : "en-us"
-});
+  clockFace: 'DailyCounter',
+  language: $('.frTag').length ? 'fr-ca' : 'en-us'
+})
